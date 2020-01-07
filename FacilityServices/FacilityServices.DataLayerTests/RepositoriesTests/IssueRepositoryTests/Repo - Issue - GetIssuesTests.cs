8b19f1a7c590cfbc27ea26ba1c5150b339004bad
Orlane Vermo <orlane_v@hotmail.com>
2020-01-07 21:35:20 +0100

    Continue IssuesRepositoryTests

--- FacilityServices/FacilityServices.DataLayerTests/RepositoriesTests/IssueRepositoryTests/Repo - Issue - GetIssuesTests.cs
+++ FacilityServices/FacilityServices.DataLayerTests/RepositoriesTests/IssueRepositoryTests/Repo - Issue - GetIssuesTests.cs
@@ -3,6 +3,7 @@ using FacilityServices.DataLayer.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using OnlineServices.Shared.FacilityServices.Exceptions;
 using OnlineServices.Shared.FacilityServices.Interfaces.Repositories;
 using OnlineServices.Shared.FacilityServices.TransfertObjects;
 using OnlineServices.Shared.TranslationServices.TransfertObjects;
@@ -13,30 +14,23 @@ using System.Reflection;
 namespace FacilityServices.DataLayerTests.RepositoriesTests.IssueRepositoryTests
 {
     [TestClass]
-    public class AddIssuesTests
+    public class GetIssuesTests
     {
         [TestMethod()]
-        public void AddIssues_ThrowsException_WhenANonExistingIdIsProvided()
+        public void GetIssueById_ThrowsException_WhenInvalidIdIsProvided()
         {
             var options = new DbContextOptionsBuilder<FacilityContext>()
                 .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
                 .Options;
             using (var memoryCtx = new FacilityContext(options))
             {
-                var IssueToUseInTest = new IssueTO
-                {
-                    Description = "prout",
-                    Name = new MultiLanguageString("Issue1EN", "Issue1FR", "Issue1NL")
-                };
-
                 var issueRepository = new IssueRepository(memoryCtx);
 
-                Assert.ThrowsException<ArgumentNullException>(() => issueRepository.Add(null));
-
+                Assert.ThrowsException<NotExistingIssueException>(() => issueRepository.GetByID(84));
             }
         }
         [TestMethod()]
-        public void AddIssue_Successfull()
+        public void GetIssueById_Successfull()
         {
             var options = new DbContextOptionsBuilder<FacilityContext>()
                 .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
@@ -54,10 +48,44 @@ namespace FacilityServices.DataLayerTests.RepositoriesTests.IssueRepositoryTests
                 issueRepository.Add(IssueToUseInTest);
                 memoryCtx.SaveChanges();
 
-                Assert.AreEqual(1, issueRepository.GetAll().Count());
                 var IssueToAssert = issueRepository.GetByID(1);
                 Assert.AreEqual(1, IssueToAssert.Id);
                 Assert.AreEqual("prout", IssueToAssert.Description);
+            }
+        }
+
+        [TestMethod()]
+        public void GetAllIssues_Successfull()
+        {
+            var options = new DbContextOptionsBuilder<FacilityContext>()
+                .UseInMemoryDatabase(databaseName: MethodBase.GetCurrentMethod().Name)
+                .Options;
+            using (var memoryCtx = new FacilityContext(options))
+            {
+                var IssueToUseInTest = new IssueTO
+                {
+                    Description = "prout",
+                    Name = new MultiLanguageString("Issue1EN", "Issue1FR", "Issue1NL")
+                };
+
+                var IssueToUseInTest2 = new IssueTO
+                {
+                    Description = "proutprout",
+                    Name = new MultiLanguageString("Issue2EN", "Issue2FR", "Issue2NL")
+                };
+                var IssueToUseInTest3 = new IssueTO
+                {
+                    Description = "proutproutprout",
+                    Name = new MultiLanguageString("Issue3EN", "Issue3FR", "Issue3NL")
+                };
+                var issueRepository = new IssueRepository(memoryCtx);
+
+                issueRepository.Add(IssueToUseInTest);
+                issueRepository.Add(IssueToUseInTest2);
+                issueRepository.Add(IssueToUseInTest3);
+                memoryCtx.SaveChanges();
+
+                Assert.AreEqual(3, issueRepository.GetAll().Count());
             }
         }
     }
