9b10e70d33579c2745b04b46e8a21c78b29639e7
Jinai <jinai.git@gmail.com>
2020-01-08 17:05:00 +0100

    Add user ID to Incident

--- Cross-Cutting/OnlineServices.Shared/FacilityServices/TransfertObjects/IncidentTO.cs
+++ Cross-Cutting/OnlineServices.Shared/FacilityServices/TransfertObjects/IncidentTO.cs
@@ -8,6 +8,7 @@ namespace OnlineServices.Shared.FacilityServices.TransfertObjects
     public class IncidentTO : IEntity<int>
     {
         public int Id { get; set; }
+        public int UserId { get; set; }
         public ComponentTO Component { get; set; }
         public IssueTO Issue { get; set; }
         public List<CommentTO> AssistantComments { get; set; } = new List<CommentTO>();
