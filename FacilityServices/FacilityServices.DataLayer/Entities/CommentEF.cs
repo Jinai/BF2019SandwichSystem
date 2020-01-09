7df7bdc11cc11e4c0d212eff98404811d2d309de
Orlane <orlane_v@hotmail.com>
2020-01-09 09:19:27 +0100

    Adding CommentEF and ComponentTypeEF

---
+++ FacilityServices/FacilityServices.DataLayer/Entities/CommentEF.cs
@@ -0,0 +1,19 @@
+﻿using OnlineServices.Shared.DataAccessHelpers;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FacilityServices.DataLayer.Entities
+{
+    [Table("Comments")]
+    public class CommentEF : IEntity<int>
+    {
+        [Key]
+        public int Id { get; set; }
+        // TODO : User from UserServices
+        //public IUser User { get; set; }
+        public IncidentEF Incident { get; set; }
+        public string Message { get; set; }
+        public DateTime SubmitDate { get; set; }
+    }
+}
