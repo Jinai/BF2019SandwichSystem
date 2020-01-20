e1c96158525b150987d7bc651a7e6d0196e0a8be
Orlane <orlane_v@hotmail.com>
2020-01-20 15:24:57 +0100

    Attendee - GetComponentTypesByRoom - 1Test NOT OK 2 tests OK

---
+++ Application Layer/FacilityServices/FacilityServices.BusinessLayer/UseCases/Attendee/Attendee - GetComponentTypesByRoom.cs
@@ -0,0 +1,24 @@
+﻿using FacilityServices.BusinessLayer.Extensions;
+using OnlineServices.Common.Exceptions;
+using OnlineServices.Common.FacilityServices.TransfertObjects;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FacilityServices.BusinessLayer.UseCases
+{
+    public partial class AttendeeRole
+    {
+        public List<ComponentTypeTO> GetComponentTypesByRoom(RoomTO roomTO)
+        {
+            if (roomTO is null)
+            {
+                throw new System.ArgumentNullException(nameof(roomTO));
+            }
+            if (roomTO.Id <= 0)
+            {
+                throw new LoggedException("The ComponentTypes cannot be reached without existing Room ID");
+            }
+            return unitOfWork.ComponentTypeRepository.GetComponentTypesByRoom(roomTO);                                                 
+        }
+    }
+}
